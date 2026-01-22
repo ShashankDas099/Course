e8c358c614a7ed32baf8364d191e71be89a57d1e
Shashank Das <shashankdas319@gmail.com>
2026-01-22 21:09:18 +0530

    Convert practice from submodule to normal folder

---
+++ practice/practiceq1.java
@@ -0,0 +1,16 @@
+class practiceq1 
+{
+    public static void main (String[] args)
+    {
+        int a = 25;
+        int b = 10;
+        int add = a+b;
+        int diff = a-b;
+        int product = a*b;
+        double quotient = (double)a/b;
+        System.out.println(add);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
---
+++ practice/practiceq2.java
@@ -0,0 +1,17 @@
+class practiceq2
+{
+    public static void main(String[] args)
+    {
+        double eng = 23.5;
+        double mat = 24.5;
+        double sci = 24;
+        double comp = 23;
+        double hin = 24.25;
+        double total = eng+mat+sci+comp+hin;
+        int totalint = (int)total;
+        double avg= total/5.0;
+        System.out.println(total);
+        System.out.println(totalint);
+        System.out.println(avg);
+    }
+}
