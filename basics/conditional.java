a47c3163ac37cf91f3b929df4ca478d1396f767a
Shashank Das <shashankdas319@gmail.com>
2026-01-23 21:03:31 +0530

    learned basic conditional statement

---
+++ basics/conditional.java
@@ -0,0 +1,19 @@
+class conditional 
+{
+    public static void main(String[] args)
+    {
+        int age = 50;
+        
+        if (age>=60){
+            System.out.println("You are an old person");
+        }
+        else if(age>=18 )
+        {
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are a child");
+        }
+        
+    }
+}
