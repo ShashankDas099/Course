8c9c6d85ed48f7ec1f07d0ff9832eff1c519ce11
Shashank Das <shashankdas319@gmail.com>
2026-01-23 20:43:37 +0530

    learned basic array

---
+++ basics/array.java
@@ -0,0 +1,9 @@
+class array 
+{
+    public static void main (String[] args){
+        String[] fruits = {"Apple","Banana","Orange","Mango"};
+        //fruits[1]="kiwi";
+        int numoffruits = fruits.length;
+        System.out.println(numoffruits);
+    }
+}
