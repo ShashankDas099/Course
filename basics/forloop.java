da8381812429e0ea5b92067a45f632b17cc75468
Shashank Das <shashankdas319@gmail.com>
2026-01-25 19:39:49 +0530

    learned about for loop

---
+++ basics/forloop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class forloop
+{
+    public static void main(String[] args){
+        //for(int i=1;i<=10;i+=2){
+            //System.out.println(i);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter how many times you want to loop: ");
+            int max = scanner.nextInt();
+            for(int i =1;i<=max;i++){
+                System.out.println(i);
+
+            }
+
+
+
+            scanner.close();
+        
+    }
+}
