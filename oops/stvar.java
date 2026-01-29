f2def24fb4c8eae14291834a2fc3acc9a25c1382
Shashank Das <shashankdas319@gmail.com>
2026-01-29 22:07:01 +0530

    learned about static methods

--- oops/stvar.java
+++ oops/stvar.java
@@ -2,8 +2,8 @@ class Mobile{
     String company;
     int price;
     static String type;
-    public void show(){
-        System.out.println("Company: "+company+",Price: "+ price+",Type: "+ type);
+    public static void show(Mobile n){
+        System.out.println("Company: "+n.company+",Price: "+ n.price+",Type: "+ n.type);
     }
 }
 public class stvar{
@@ -12,10 +12,10 @@ public class stvar{
         Mobile obj1 = new Mobile();
         obj1.company="Apple";
         obj1.price=1500;
-        obj1.show();
         Mobile obj2 = new Mobile();
         obj2.company="Samsung";
         obj2.price=1100;
-        obj2.show();
+        Mobile.show(obj1);
+        Mobile.show(obj2);
     }
 }
