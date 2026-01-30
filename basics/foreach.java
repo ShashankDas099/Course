da112ae7a55c50091c87e989ac1678e9410d965e
Shashank Das <shashankdas319@gmail.com>
2026-01-30 08:59:10 +0530

    learned foreach loop or enhanced for loop

---
+++ basics/foreach.java
@@ -0,0 +1,8 @@
+public class foreach{
+    public static void main(String[] args){
+        String animals[]= {"cat","dog","lion","tiger"};
+        for (String n : animals){
+            System.out.println(n);
+        }
+    }
+}
