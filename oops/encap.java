398d4340273cecccb76b9e60db843b012d0d6e92
Shashank Das <shashankdas319@gmail.com>
2026-01-30 09:19:19 +0530

    learned about this keyword

--- oops/encap.java
+++ oops/encap.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class human{
     private String name;
     private int age;
-    public void setAge(int a){
-        age=a;
+    public void setAge(int age){
+        this.age=age;
     }
-    public void setName(String n){
-        name=n;
+    public void setName(String name){
+        this.name=name;
     }
     public int getAge(){
         return age;
@@ -24,7 +24,15 @@ public class encap{
         scanner.nextLine();
         System.out.println("Enter Name: ");
         h1.setName(scanner.next());
+
+        human h2 = new human();
+        System.out.println("Enter Age: ");
+        h2.setAge(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Enter Name: ");
+        h2.setName(scanner.next());
         System.out.println(h1.getAge()+":"+h1.getName());
+        System.out.println(h2.getAge()+":"+h2.getName());
 
     }
 }
